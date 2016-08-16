23bf5851ff8b33704965b9ae98a6f3c33f340bb2
DShaw14 <david14shaw@gmail.com>
2016-08-16 15:11:37 -0400

    First actual layout of android app

--- app/src/main/java/oaklabs/supportal/UserLogin.java
+++ app/src/main/java/oaklabs/supportal/UserLogin.java
@@ -1,9 +1,9 @@
 package oaklabs.supportal;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class UserLogin extends AppCompatActivity {
+public class UserLogin extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
