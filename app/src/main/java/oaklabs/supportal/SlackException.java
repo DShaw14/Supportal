32fd520852fd4bb5040cc40d3b70a861281cabdf
sureshmachetti <vsk@umich.edu>
2016-09-12 13:56:31 -0400

    Added slack-webhook

---
+++ app/src/main/java/oaklabs/supportal/SlackException.java
@@ -0,0 +1,11 @@
+package oaklabs.supportal;
+
+/**
+ * Created by sureshmachetti on 9/12/16.
+ */
+public class SlackException extends RuntimeException {
+
+    public SlackException(Throwable cause) {
+        super(cause);
+    }
+}
