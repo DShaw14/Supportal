2ccf08c1a3c3c0cd92bed906337995299c8c29fc
sureshmachetti <vsk@umich.edu>
2016-09-12 13:03:20 -0400

    Added Simple Slack API

--- app/src/main/java/oaklabs/supportal/SubmitIssue.java
+++ app/src/main/java/oaklabs/supportal/SubmitIssue.java
@@ -13,6 +13,15 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import org.json.JSONObject;
 
+import com.ullink.slack.simpleslackapi.SlackChannel;
+import com.ullink.slack.simpleslackapi.SlackSession;
+import com.ullink.slack.simpleslackapi.SlackUser;
+import com.ullink.slack.simpleslackapi.events.SlackMessagePosted;
+import com.ullink.slack.simpleslackapi.impl.SlackChatConfiguration;
+import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory;
+import com.ullink.slack.simpleslackapi.listeners.SlackMessagePostedListener;
+
+
 public class SubmitIssue extends Activity {
 
     EditText nameEdit;
@@ -47,4 +56,78 @@ public class SubmitIssue extends Activity {
         }
 
     }*/
+
+        private static String slackAPIKey = "xoxp-63069436370-63053733136-77700959316-30a9396b86";
+        private static SlackSession slackSession;
+        private static SubmitIssue simpleSlackAPITest;
+
+        public SubmitIssue(String[] args) {
+            System.out.println("Constructing SimpleSlackAPITest");
+            simpleSlackAPITest = this;
+            run(args);
+        }
+
+        public static void main(String[] args) {
+            System.out.println("Hello world, SimpleSlackAPITest here.");
+            new SubmitIssue(args);
+        }
+
+        public static void run(String[] args) {
+            System.out.println("SimpleSlackAPITest > run > Slack Init");
+        /* SLACK INIT */
+            slackSession = SlackSessionFactory.createWebSocketSlackSession(slackAPIKey);
+            try {
+                slackSession.connect();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+
+
+            SlackEvents slackevents = simpleSlackAPITest.new SlackEvents();
+            slackSession.addMessagePostedListener(slackevents);
+
+            while (true) {
+                System.out.println("loop");
+                simpleSlackAPITest.sendMessageToAChannel(slackSession);
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public class SlackEvents implements SlackMessagePostedListener {
+
+            @Override
+            public void onEvent(SlackMessagePosted event, SlackSession session) {
+                // Ignore self.
+                if (session.sessionPersona().getId().equals(event.getSender().getId())) {
+                    System.out.println("Ignoring self.");
+                    return;
+                } else {
+                    System.out.println("Not Self");
+                    System.out.println(session.sessionPersona().getId());
+                    System.out.println(event.getSender().getId());
+                }
+
+                SlackChannel channelOnWhichMessageWasPosted = event.getChannel();
+                String messageContent = event.getMessageContent();
+                SlackUser messageSender = event.getSender();
+
+                System.out.println(channelOnWhichMessageWasPosted.getName() + ":" + messageSender.getUserName() + ":"
+                        + messageContent);
+            }
+        }
+
+        public void sendMessageToAChannel(SlackSession session) {
+
+            // get a channel
+            SlackChannel channel = session.findChannelByName("test1");
+
+            session.sendMessage(channel, "test message");
+            session.sendMessage(channel, "test message with name", null,
+                    SlackChatConfiguration.getConfiguration().withName("Zach"));
+        }
 }
